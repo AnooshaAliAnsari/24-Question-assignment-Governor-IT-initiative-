4e786119b4324d471ab77e25963b4b81c5902118
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:10:02 +0500

    Add files via upload

---
+++ Q-10/main.ts
@@ -0,0 +1,7 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//exercise 10
+//my name is anoosha 
+//date:23/march/2024
+//this programe print a persnol message
+console.log("Hello Eric would you like a python");
