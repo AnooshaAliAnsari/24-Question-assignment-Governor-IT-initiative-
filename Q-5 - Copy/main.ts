00c73565fdaa10a2dfa943eca93c30a34218bc32
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:06:16 +0500

    Add files via upload

---
+++ Q-5 - Copy/main.ts
@@ -0,0 +1,9 @@
+/*Albert Einstein once said, “Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.*/
+
+
+let famous_person:string="Albert Einstein";
+let message:string=`${famous_person} once said, "A person who said who never made a mistake never tried anything new."`;
+console.log(message);
