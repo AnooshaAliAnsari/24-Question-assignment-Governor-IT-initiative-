51887c1cab07cff28e4e7ea66fca2fc25579672e
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:16:55 +0500

    Add files via upload

---
+++ Q-24/main.js
@@ -0,0 +1,16 @@
+"use strict";
+/*More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+• Tests for equality and inequality with strings*/
+let apple = "APPLE";
+console.log(apple == "apple");
+console.log(apple == "APPLE");
+//• Tests using the lower case function
+console.log("APPLE".toLowerCase() == "apple");
+console.log("APPLE".toLocaleUpperCase() == "apple");
+//Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+let numb = 2;
+console.log(2 < 3);
+console.log(2 > 1);
+let num1 = 2 > 4;
+console.log(num1 == 2 < 1);
+console.log(num1 == 2 < 5);
