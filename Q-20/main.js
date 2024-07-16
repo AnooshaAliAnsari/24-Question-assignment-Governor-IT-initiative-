079e3d171b5b2c94dcbda8249400c5c09bfff7dc
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:15:19 +0500

    Add files via upload

---
+++ Q-20/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let languages = ["urdu", "English", "chines", "spanish", "turkish"];
+console.log("           list of languages          :");
+for (let top of languages) {
+    console.log(`* ` + top);
+}
