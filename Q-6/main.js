91095e6ef3dc2c795aef08b7c7896921f8e6a39f
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:08:31 +0500

    Add files via upload

---
+++ Q-6/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+/*  \t (means tab button"4 word space")     /n(means next line).*/
+let personName = "\n\t  anoosha ali ansari \t\n";
+console.log(personName);
+console.log(personName.trim());
