1bea749a55e1767cb2099c34ebeb42ab15361fe8
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:02:56 +0500

    Add files via upload

---
+++ Q-3/main.ts
@@ -0,0 +1,8 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName : string= "Anoosha ali ansari";
+console.log ("lowercase:", personName.toLocaleLowerCase())
+
+console.log("uppercase:", personName.toUpperCase())
+
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase() ));
