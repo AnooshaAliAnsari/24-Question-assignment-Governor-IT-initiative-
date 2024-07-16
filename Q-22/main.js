345f45a3082772fc20863722460c0c86ceda86c3
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:16:20 +0500

    Add files via upload

---
+++ Q-22/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Intentional Error:
+/*let arr:(string | number) []=["tea","cold-drink","cofee" ,1 ,4 ,7,];
+console.log(arr[6]);
+console.log([3])*/
+let array = ["a", "b", "c"];
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    console.log([2]);
+}
+;
---
+++ Q-22/main.ts
@@ -0,0 +1,6 @@
+//Intentional Error:
+let arr:(string | number) []=["tea","cold-drink","cofee" ,1 ,4 ,7,];
+console.log(arr[6]);
+console.log([3])
+
+
