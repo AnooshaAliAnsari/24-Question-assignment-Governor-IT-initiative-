b28901ce9afbfaf532fc9091833d659323a5d363
ANOOSHA ALI ANSARI <164379757+AnooshaAliAnsari@users.noreply.github.com>
2024-07-16 18:16:39 +0500

    Add files via upload

---
+++ Q-23/main.ts
@@ -0,0 +1,17 @@
+let car = 'subaru';
+
+
+//Test 1: Equality comparison (true)
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+//Test 2: strict equality comprison (True)
+console.log("is car ==='subaru' ? I predict true")
+console.log(car === "subaru")
+
+//Test 3: Inequality comparison (false)
+console.log("is car != 'subaru' ? I predict False")
+console.log(car != "subaru")
+
+console.log(car =="toyota")
